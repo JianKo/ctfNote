c8e891fc95740ca49eed8b750c3b4e3a67e38514
bluetoonist <bluetoon@naver.com>
2020-06-22 23:36:07 -0700

    [+]Update /gdb_study

---
+++ study_hacked/system_tip/gdb_study/Example01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+ int var1 = 2319;
+ int var2 = 4983;
+
+
+ if( var1 < var2) {
+   printf("Var2 is bigger\n");
+ }
+ 
+ printf("[-] Program has exit\n");
+ return 0;
+}
